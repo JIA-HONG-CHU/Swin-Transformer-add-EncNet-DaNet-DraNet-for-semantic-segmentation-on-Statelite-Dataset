15fda6ec25cabf3cb24b19b0f1eb816380bbd90e
JIA-HONG-CHU <56266563+JIA-HONG-CHU@users.noreply.github.com>
2022-05-14 00:06:30 +0800

    Update __init__.py

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -8,8 +8,10 @@ from .resnet import ResNet, ResNetV1c, ResNetV1d
 from .resnext import ResNeXt
 from .unet import UNet
 from .swin_transformer import SwinTransformer
+from .swin_transformerV2 import SwinTransformerV2
 
 __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
-    'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3', 'SwinTransformer'
+    'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3', 'SwinTransformer','SwinTransformerV2'
 ]
+
